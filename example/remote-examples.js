f32111f34d09aea4af063223137d7c1a5a8d556d
Mike Deem <mike@maimedleech.com>
2011-05-24 22:52:57 -0700

    checkpoint

---
+++ example/remote-examples.js
@@ -0,0 +1,25 @@
+var toddick = require('toddick');
+var remote = require('toddick/lib/remote');
+
+var PingPong = toddick(
+  {
+    PING: function(PONG) {
+      PONG();
+    }
+  }
+);
+
+var Pinger = toddick(
+  {
+    PING: function(pingpong) {
+      pingpong.PING(this.PONG);
+    },
+    PONG: function() {
+      console.log("PONG!");
+    }
+  }
+);
+
+new remote.HttpMilieu(8910).PUBLISH("/pingpong", new PingPong());
+
+new remote.HttpMilieu(8911).PROXY("http://localhost:8910/pingpong", new Pinger().PING);
